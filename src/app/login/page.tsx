7d6bf5fa8bb80ad3f95db71360fafc16c717f94f
Fernando Estrada <feresdev@gmail.com>
2025-10-02 22:30:15 -0400

    fix(ui): corrige dimenciones reales del logo y elimina prop innecesaria en Logo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,7 +6,9 @@ export default function LoginPage() {
     <section className="flex w-full h-screen items-center justify-center p-5">
       <div className="w-[400px] px-8 py-16 bg-dark rounded-xl border-t-primary border-t-4 border-border">
         <div className="flex px-6 flex-col items-center justify-center">
-          <Logo width={200} height={232} isWhite altura={'w-full'} />
+          <div className='flex h-12'>
+            <Logo width={571} height={142} isWhite />
+          </div>
           <h1 className="text-title text-center py-4 text-xl font-semibold">Inicia sesión</h1>
         </div>
         <FormLogin />
--- src/components/illustations/Logo.js
+++ src/components/illustations/Logo.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 
-export default function Logo({ width, height, altura, isWhite }) {
+export default function Logo({ width, height, isWhite }) {
   return (
-    <Image src={`${!isWhite ? '/Streemo-logo-dark.svg' : '/Streemo-logo-white.svg'}`} width={width} height={height} alt="Lg Streemo" className={`h-${altura}`} />
+    <Image src={`${!isWhite ? '/Streemo-logo-dark.svg' : '/Streemo-logo-white.svg'}`} width={width} height={height} alt="Lg Streemo" />
   )
 }
